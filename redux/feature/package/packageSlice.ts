01326a6ad88fc93ec849d6e4ead8184eff777ea4
aryan <aryan@codelamda.com>
2025-09-01 15:43:49 +0530

    added edit and delete package

--- redux/feature/package/packageSlice.ts
+++ redux/feature/package/packageSlice.ts
@@ -3,22 +3,24 @@ import { Status } from "@lib/constants/enum";
 import { Package } from "./IPackageState";
 import {
   createPackage,
+  deletePackage,
   fetchPackageItems,
   fetchPackages,
+  updatePackage,
 } from "./packageThunk";
 import { Item } from "../masterPriceList/iMasterPriceListState";
 
 interface PackageState {
   packages: Package[] | null;
   items: Item[] | null;
-  status: { packages: Status; items: Status;create: Status };
+  status: { packages: Status; items: Status; item: Status };
   selectedFilters: { range: string; dwelling_type: string };
 }
 
 const initialState: PackageState = {
   packages: null,
   items: null,
-  status: { packages: Status.IDLE, items: Status.IDLE,create: Status.IDLE },
+  status: { packages: Status.IDLE, items: Status.IDLE, item: Status.IDLE },
   selectedFilters: { range: '', dwelling_type: '' },
 };
 
@@ -47,15 +49,42 @@ const packageSlice = createSlice({
     });
     //create
     builder.addCase(createPackage.pending, (state) => {
-      state.status.create = Status.PENDING;
+      state.status.item = Status.PENDING;
     });
     builder.addCase(createPackage.fulfilled, (state, action) => {
-      state.status.create = Status.SUCCESS;
+      state.status.item = Status.SUCCESS;
       state.packages.unshift(action.payload);
     });
     builder.addCase(createPackage.rejected, (state) => {
-      state.status.create = Status.ERROR;
+      state.status.item = Status.ERROR;
     });
+    //update
+    builder.addCase(updatePackage.pending, (state) => {
+      state.status.item = Status.PENDING;
+    });
+    builder.addCase(updatePackage.fulfilled, (state, action) => {
+      state.status.item = Status.SUCCESS;
+      state.packages = state.packages?.map((pkg) => {
+        if (pkg.packageId === action.payload.packageId) {
+          return action.payload;
+        }
+        return pkg;
+      });
+    });
+    builder.addCase(updatePackage.rejected, (state) => {
+      state.status.item = Status.ERROR;
+    });
+    //delete
+    builder.addCase(deletePackage.pending, (state) => {
+      state.status.item = Status.PENDING;
+    });
+    builder.addCase(deletePackage.fulfilled, (state, action) => {
+      state.status.item = Status.SUCCESS;
+      state.packages = state.packages?.filter((pkg) => pkg.packageId !== action.payload.id);
+    });
+    builder.addCase(deletePackage.rejected, (state) => {
+      state.status.item = Status.ERROR;
+    }); 
     //get items
     builder.addCase(fetchPackageItems.pending, (state) => {
       state.status.items = Status.PENDING;
