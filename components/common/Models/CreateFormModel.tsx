9bfa417d9961f0696e75390d928b7f674e00abea
Akshay CL <akshaycodelamda@gmail.com>
2025-09-03 16:12:01 +0530

    fixed create form issue which reseting the form on the error

--- components/common/Models/CreateFormModel.tsx
+++ components/common/Models/CreateFormModel.tsx
@@ -64,14 +64,13 @@ export const CreateFormModal: React.FC<CreateFormModalProps> = ({
 
   useEffect(() => {
     if (open) {
-      if (isEditing) {
+      if (isEditing && initialValues) {
         form.setFieldsValue(initialValues);
-      }
-       else {
+      } else if (!isEditing) {
         form.resetFields();
       }
     }
-  }, [open, isEditing, initialValues, form]);
+  }, [open, isEditing]);
 
   const handleOk = async () => {
     try {
