105bb3bc475a186a5d7f105e47a7841eddfd2dd3
aryan <aryan@codelamda.com>
2025-09-03 15:41:33 +0530

    fix minor issue

--- pages/setting/components/FloorPlan.tsx
+++ pages/setting/components/FloorPlan.tsx
@@ -70,9 +70,8 @@ const FloorPlan = () => {
       formData.append("image", values.image.fileList[0].originFileObj);
       await dispatch(createFloorPlan(formData))
         .unwrap()
-        .then((response) => console.log(response))
-        .catch((err) => console.error(err));
       setIsModalVisible(false);
+      message.success("Floor Plan created successfully");
     } catch (error) {
       message.error(error || 'Failed to create Floor Plan')
     } finally {
