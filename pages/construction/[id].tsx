fa24fba27ca2fca091f8ca733e066d0cfb72c171
Jenil <jenillaheri.fameux@gmail.com>
2025-09-29 11:28:48 +0530

    Some chanfes

--- pages/construction/[id].tsx
+++ pages/construction/[id].tsx
@@ -4,7 +4,6 @@ import StageProgress from '@/components/common/StageProgress'
 import ConstructionDetailHeader from '@/components/construction/ConstructionDetailHeader'
 import ConstructionTimeline from '@/components/construction/ConstructionTimeline'
 import ConstructionTabs from '@/components/construction/ConstructionTab'
-import MailSendModal from '@/components/common/Models/MailSendModal'
 
 const index = () => {
     const router = useRouter()
@@ -31,13 +30,6 @@ const index = () => {
             </div>
             <ConstructionTimeline current_value={current} setCurrent={setCurrent} steps={steps}  />
             <ConstructionTabs current_value={current}/>
-
-       <MailSendModal
-        open={true}
-        onCancel={()=>{}}
-        onSend={()=>{}}
-        title="Send email to Private Inspector"
-      />
         </div>
     )
 }
