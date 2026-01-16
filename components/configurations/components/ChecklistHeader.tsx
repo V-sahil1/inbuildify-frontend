a58efce39db761e14df38278acb1ed7f81b80900
maitri <maitri@codelamda.com>
2026-01-16 11:22:20 +0530

    fix minor changes in checklist header

--- components/configurations/components/ChecklistHeader.tsx
+++ components/configurations/components/ChecklistHeader.tsx
@@ -10,40 +10,64 @@ export const ChecklistHeader = ({ onChange, data }) => {
   const { stageOptions } = useConstructionStageHook();
 
   useEffect(() => {
-    if (builderOptions && typeOptions && stageOptions && !data.builder && !data.constructionType && !data.constructionStage) {
+    if (
+      builderOptions &&
+      typeOptions &&
+      stageOptions &&
+      !data.builder &&
+      !data.constructionType &&
+      !data.constructionStage
+    ) {
       onChange({
         builder: builderOptions[0].value,
         constructionType: typeOptions[0].value,
-        constructionStage: stageOptions[0].value
+        constructionStage: stageOptions[0].value,
       });
     }
-  }, [builderOptions, typeOptions, stageOptions, data.builder, data.constructionType, data.constructionStage, onChange]);
+  }, [
+    builderOptions,
+    typeOptions,
+    stageOptions,
+    data.builder,
+    data.constructionType,
+    data.constructionStage,
+    onChange,
+  ]);
   return (
     <Form layout="vertical" className="mb-6">
       <Row gutter={16}>
         <Col span={8}>
-          <Select
-            value={data.builder}
-            placeholder="Select Builder"
-            options={builderOptions}
-            onChange={val => onChange(prev => ({ ...prev, builder: val }))}
-          />
+          <div className="flex gap-2 items-center">
+            <p>Builder</p>
+            <Select
+              value={data.builder}
+              placeholder="Select Builder"
+              options={builderOptions}
+              onChange={val => onChange(prev => ({ ...prev, builder: val }))}
+            />
+          </div>
         </Col>
         <Col span={8}>
-          <Select
-            value={data.constructionType}
-            placeholder="Select Construction Type"
-            options={typeOptions}
-            onChange={val => onChange(prev => ({ ...prev, constructionType: val }))}
-          />
+          <div className="flex gap-2 items-center">
+            <p>Construction Type </p>
+            <Select
+              value={data.constructionType}
+              placeholder="Select Construction Type"
+              options={typeOptions}
+              onChange={val => onChange(prev => ({ ...prev, constructionType: val }))}
+            />
+          </div>
         </Col>
         <Col span={8}>
-          <Select
-            value={data.constructionStage}
-            placeholder="Select Stage"
-            options={stageOptions}
-            onChange={val => onChange(prev => ({ ...prev, constructionStage: val }))}
-          />
+          <div className="flex gap-2 items-center">
+            <p>Construction Stage </p>
+            <Select
+              value={data.constructionStage}
+              placeholder="Select Stage"
+              options={stageOptions}
+              onChange={val => onChange(prev => ({ ...prev, constructionStage: val }))}
+            />
+          </div>
         </Col>
       </Row>
     </Form>
